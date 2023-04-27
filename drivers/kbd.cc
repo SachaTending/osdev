fcc830897e9d4cdaa8d99754a95f4df8442e5105
SachaTending <65766775+SachaTending@users.noreply.github.com>
2023-04-27 21:31:03 +0300

    Update kbd.cc

--- drivers/kbd.cc
+++ drivers/kbd.cc
@@ -79,12 +79,73 @@ void kbd_enable() {
     inb(0x60);
     kbd_in_init = false;
 }
+char kbdus[128] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', /* 9 */
+    '9', '0', '-', '=', '\b',   /* Backspace */
+    '\t',           /* Tab */
+    'q', 'w', 'e', 'r', /* 19 */
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',       /* Enter key */
+    0,          /* 29   - Control */
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',   /* 39 */
+    '\'', '`',   0,     /* Left shift */
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n',         /* 49 */
+    'm', ',', '.', '/',   0,                    /* Right shift */
+    '*',
+    0,  /* Alt */
+    ' ',    /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+    '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+    '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
+char kbd_buf[1024];
+int kbd_buf_p = 0;
+#include "libc.h"
+void kbd_handle() {
+    uint8_t scancode = inb(0x60);
+    if (scancode & 0x80) {
+        return;
+    }
+    printf("%c", kbdus[scancode]);
+    // Append char to buffer
+    if (kbdus[scancode] == '\n') {
+        printf("\n");
+        kbd.log("Buffer = %s\n", kbd_buf);
+        memset((void *)kbd_buf, '\0', 1024);
+        kbd_buf_p = 0;
+        return;
+    }
+    kbd_buf[kbd_buf_p] = kbdus[scancode];
+    kbd_buf_p++;
+}
 void kbd_handl(stackframe_t *reg) {
     if (kbd_in_init);
-    else inb(0x60);
+    else kbd_handle();
 }
 void kbd_init() {
-    kbd.log("Initializating...\n");\
+    kbd.log("Initializating...\n");
+    for (int i=0;i<1024;i++) {
+        kbd_buf[i] = '\0';
+    }
     idt_set_handl(1, kbd_handl);
     return;
     // Get device type
