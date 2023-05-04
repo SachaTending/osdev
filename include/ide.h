014b4e421ec720610688fc62861ddd1a9ce3ea1d
SachaTending <65766775+SachaTending@users.noreply.github.com>
2023-05-04 13:29:34 +0300

    Update ide.h

--- include/ide.h
+++ include/ide.h
@@ -5,3 +5,11 @@ struct ide_dev {
     uint8_t bus, drive;
 
 };
+
+void ide_select_drive(uint8_t bus, uint8_t drive);
+void ide_select_drive(ide_dev dev);
+void ide_select_drive(ide_dev *dev);
+
+void ide_poll(uint16_t io);
+void ide_soft_reset(uint8_t io);
+void ide_400ns(uint16_t io);
